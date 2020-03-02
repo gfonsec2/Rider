a81fb465277618e658c16d00b8c419f13089f7b8
Gilbert Fonseca <gilbertfonseca@Gilberts-MacBook-Pro.local>
2020-03-02 09:51:25 -0600

    Mph zeros out

--- lib/QuickStart.dart
+++ lib/QuickStart.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Home.dart';
-import 'Settings.dart';
-import 'Records.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 final databaseReference = FirebaseDatabase.instance.reference().child("user").child('rotations_per_minute_stream').child('RPM');
@@ -66,54 +63,52 @@ class _QuickStartState extends State<QuickStart> {
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-            Container(
-              height: 300.0,
-              width: 300.0,
-              decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xffFFCC00), Color(0xffFF6666)], 
+          Container(
+            height: 300.0,
+            width: 300.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xffFFCC00), Color(0xffFF6666)], 
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-            Container(
-              height: 285.0,
-              width: 285.0,
-              decoration: BoxDecoration(
+          ),
+          Container(
+            height: 285.0,
+            width: 285.0,
+            decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                   new StreamBuilder(
-                      stream: databaseReference.onValue,
-                      builder: (context, snap) {
-                            if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null)
-                            {
-                              DataSnapshot snapshot = snap.data.snapshot;
-                              var value = snapshot.value;
-                              mph = double.parse(value.toString());
-                              mph = (mph * diameter * 3.14159 *60/63360);
-
-                              return Text( mph.toInt().toString(),
-                              style: TextStyle(fontSize: 80)
-                                );
-                            }
-                            else
-                        {
-                          return Text("i am not reading");
-                        }
-                        }
-                      ,),
-                  Text("MPH",
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                StreamBuilder(
+                  stream: databaseReference.onValue,
+                  builder: (context, snap) {
+                    if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null){
+                      DataSnapshot snapshot = snap.data.snapshot;
+                      var value = snapshot.value;
+                      mph = double.parse(value.toString());
+                      mph = (mph * diameter * 3.14159 *60/63360);
+                      return Text( mph.toInt().toString(),
+                        style: TextStyle(fontSize: 80)
+                      );
+                    }
+                    else{
+                      return Text("i am not reading");
+                    }
+                  }
+                ),
+                Text("MPH",
                   style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300),
-                  )
-                ],
-              ),
-            )
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -128,116 +123,114 @@ class _QuickStartState extends State<QuickStart> {
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-            Container(
-              height: 125.0,
-              width: 125.0,
-              decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xff916DD5), Color(0xff66CCCC)]),
+          Container(
+            height: 125.0,
+            width: 125.0,
+            decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff916DD5), Color(0xff66CCCC)]),
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-            Container(
-              height: 110.0,
-              width: 110.0,
-              decoration: BoxDecoration(
+          ),
+          Container(
+            height: 110.0,
+            width: 110.0,
+            decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-                      Container(
-              child: Column(
-                children: <Widget>[
-                  //Mau, Insert int _calories
-                  new StreamBuilder(
-                      stream: databaseReferencePulses.onValue,
-                      builder: (context, snap) {
-                            if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null)
-                            {
-                              DataSnapshot snapshot = snap.data.snapshot;
-                              var value = snapshot.value;
-                              distance = value.toInt()*50*3.14159/63360;
-
-                              return Text( distance.toStringAsFixed(2),
-                              style: TextStyle(fontSize: 45)
-                                );
-                            }
-                            else
-                        {
-                          return Text("0",
-                          style: TextStyle(fontSize: 20));
-                        }
-                        }
-                      ,),
-                  Text("Miles",
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                StreamBuilder(
+                  stream: databaseReferencePulses.onValue,
+                  builder: (context, snap) {
+                    if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null){
+                      DataSnapshot snapshot = snap.data.snapshot;
+                      var value = snapshot.value;
+                      distance = value.toInt()*50*3.14159/63360;
+                      return Text(
+                        distance.toStringAsFixed(1),
+                        style: TextStyle(fontSize: 45)
+                      );
+                    }
+                    else{
+                      return Text("0",
+                      style: TextStyle(fontSize: 45));
+                    }
+                  }
+                ),
+                Text("Miles",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-                  )
-                ],
-              ),
-            )
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 
   Widget _cal(){
-     int calories;
-      return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(150),
-        ),
-        elevation: 20,
-        child: Stack(
+    int calories;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(150),
+      ),
+      elevation: 20,
+      child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-            Container(
-              height: 125.0,
-              width: 125.0,
-              decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xff66CCCC), Color(0xffFF6666)]),
+          Container(
+            height: 125.0,
+            width: 125.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff66CCCC), Color(0xffFF6666)]
+              ),
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-            Container(
-              height: 110.0,
-              width: 110.0,
-              decoration: BoxDecoration(
+          ),
+          Container(
+            height: 110.0,
+            width: 110.0,
+            decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.all( Radius.circular(150.0)),
-               ),
             ),
-                      Container(
-              child: Column(
-                
-                children: <Widget>[new StreamBuilder(
-                      stream: databaseReferencePulses.onValue,
-                      builder: (context, snap) {
-                            if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null)
-                            {
-                              DataSnapshot snapshot = snap.data.snapshot;
-                              var value = snapshot.value;
-                              distance = value.toInt()*50*3.14159/63360;
-                              calories = (distance*50).toInt();
-                              return Text( calories.toString(),
-                              style: TextStyle(fontSize: 45)
-                                );
-                            }
-                            else
-                        {
-                          return Text("0",
-                          style: TextStyle(fontSize: 45));
-                        }
-                        }
-                      ,),
-                 
-                  Text("Calories",
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                StreamBuilder(
+                  stream: databaseReferencePulses.onValue,
+                  builder: (context, snap) {
+                    if(snap.hasData && !snap.hasError && snap.data.snapshot.value!=null){
+                      DataSnapshot snapshot = snap.data.snapshot;
+                      var value = snapshot.value;
+                      distance = value.toInt()*50*3.14159/63360;
+                      calories = (distance*50).toInt();
+                      return Text(calories.toString(),
+                        style: TextStyle(fontSize: 45)
+                      );
+                    }
+                    else{
+                      return Text("0",
+                        style: TextStyle(fontSize: 45)
+                      );
+                    }
+                  }
+                ),
+                Text("Calories",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-                  )
-                ],
-              ),
-            )
+                )
+              ],
+            ),
+          )
         ],
-    ),
-      );
+      ),
+    );
   }
 
   Widget _stopwatch(context){
@@ -272,27 +265,27 @@ class _QuickStartState extends State<QuickStart> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-      margin: EdgeInsets.fromLTRB(15, 40, 15, 0),
-      child: Column(
-        children: <Widget>[
-          _back(),
-          _title(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              _mph(),
-                Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  _dist(),
-                  _cal(),
-                ],
-              ),
-              _stopwatch(context)
-            ],
-          ),
-        ],
-      ),
+        margin: EdgeInsets.fromLTRB(15, 40, 15, 0),
+        child: Column(
+          children: <Widget>[
+            _back(),
+            _title(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _mph(),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _dist(),
+                    _cal(),
+                  ],
+                ),
+                _stopwatch(context)
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
